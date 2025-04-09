5f079276465a0477df0dfcc7a113a31f41985329
Somil11 <somil.11.06@gmail.com>
2025-04-09 21:22:11 +0530

    useEffect dependency

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [selectedScript, setSelectedScript] = useState('');
 
-  const scriptOptions = {
+  const scriptOptions = useMemo(() => ({
     option1: '',
     option2: 'https://eucdn.whatfix.com/prod/a6b8aa9c-6196-4580-b859-cedb3075b240/initiator/initiator.nocache.js',
     option3: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js',
     option4: 'https://cdn.jsdelivr.net/npm/chart.js'
-  };
+  }), []); // Empty dependency array as these URLs don't change
 
   useEffect(() => {
     if (selectedScript) {
       const script = document.createElement('script');
-        script.language = 'javascript';
-        script.async = true;
-        script.type = 'text/javascript';
+      script.type = 'text/javascript';
+      script.async = true;
       script.src = scriptOptions[selectedScript];
       document.head.appendChild(script);
 
@@ -24,7 +23,7 @@ function Navbar() {
         document.head.removeChild(script);
       };
     }
-  }, [selectedScript]);
+  }, [selectedScript, scriptOptions]);
 
   return (
     <nav style={styles.nav}>
